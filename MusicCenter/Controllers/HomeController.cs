7cb2cb28600842bacbfad2d79e95cd60d52f80a6
lkosciuk <lkosciukpriv@gmail.com>
2015-10-16 14:40:14 +0200

    Zmiana z MVC 4 na MVC 5

--- MusicCenter/Controllers/HomeController.cs
+++ MusicCenter/Controllers/HomeController.cs
@@ -13,24 +13,5 @@ namespace MusicCenter.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-            
-            return View();
-        }
-
-        protected override void OnException(ExceptionContext filterContext)
-        {
-            base.OnException(filterContext);
-        }
     }
 }
--- MusicCenter/Global.asax.cs
+++ MusicCenter/Global.asax.cs
@@ -20,6 +20,7 @@ namespace MusicCenter
             AutofacConfig.ConfigureContainer();
             AutoMapperWebConfig.Configure();
             
+            
         }
     }
 }
