bd8e2cc96dfe54f0f43e019c2a0f3ccb2475c459
lkosciuk <lkosciukpriv@gmail.com>
2015-10-19 19:38:11 +0200

    Poprawa MVC 5

---
+++ MusicCenter.App/App_Start/AutoMapperWebConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MusicCenter.Common.ViewModels;
+using MusicCenter.Dal.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicCenter.App.App_Start
+{
+    public class AutoMapperWebConfig
+    {
+        public static void Configure()
+        {
+            ConfigureUserMapping();
+            //...
+        }
+
+        private static void ConfigureUserMapping()
+        {
+            Mapper.CreateMap<Users, UserRegisterViewModel>();
+        } 
+    }
+}
