0f214a18fed3ec42381fd6cd8a6c6d5b06def32b
lkosciuk <lkosciukpriv@gmail.com>
2015-11-16 18:28:20 +0100

    Zmiana mapowania favourites w users

--- MusicCenter.Dal/EntityConfigurations/UsersConfiguration.cs
+++ MusicCenter.Dal/EntityConfigurations/UsersConfiguration.cs
@@ -24,7 +24,7 @@ namespace MusicCenter.Dal.EntityConfigurations
             //this.HasMany(a => a.tours).WithMany(a => a.users);
             //this.HasMany(a => a.followed).WithMany(a => a.spectators);
             //this.HasMany(a => a.spectators).WithMany(a => a.followed);
-            this.HasOptional(a => a.favourites).WithOptionalDependent(a => a.user);
+            this.HasRequired(a => a.favourites).WithRequiredDependent(a => a.user);
             this.HasMany(a => a.receivedMessages).WithMany(a => a.UserReceivers);
             this.HasMany(a => a.sentMessages).WithOptional(a => a.UserAuthor);
             this.HasOptional(a => a.bandMember).WithOptionalDependent(a => a.user);
